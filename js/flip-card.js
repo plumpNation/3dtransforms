0aa3d847662acd9e87ce210f69d2f096240c7e1b
Gavin King <plumpnation.king@pearson.com>
2015-02-06 00:22:24 +0100

    Spike started on samsung android desktop animation clone

--- js/flip-card.js
+++ js/flip-card.js
@@ -1,9 +1,28 @@
-var init = function() {
-  var card = document.getElementById('card');
-  
-  document.getElementById('flip').addEventListener( 'click', function(){
-    card.toggleClassName('flipped');
-  }, false);
-};
+var card = document.getElementById('card');
 
-window.addEventListener('DOMContentLoaded', init, false);
+function setRotation(angle) { 
+  if (angle > 90) angle = 90;
+  if (angle < -90) angle = -90;
+  card.style.transform = "rotateY( " + angle + "deg )";
+}
+
+function setOrientation(x) {
+  var origin = 'right';
+
+  if (x > 0) {
+    origin = 'left';
+  }
+
+  card.style.transformOrigin = "top " + origin;
+}
+
+function setOpacity(x) {
+  card.style.opacity = Math.max(1 - Math.abs(x) / 200, 0.3);
+}
+
+Draggable.create(card, {type: 'left', onDrag: function () {
+  var angle = -(this.x / 2);
+  setRotation(angle);
+  setOrientation(this.x);
+  setOpacity(this.x);
+}, throwProps: true});
